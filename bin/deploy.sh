8f9bef4b1ffef2d58f74329f2fbc170562bb7d1a
Ben Marshall <ben.marshall@bristol.ac.uk>
2020-09-23 10:44:10 +0100

    Add how-tos for deploy and db update scripts
     On branch master
     Your branch is ahead of 'origin/master' by 1 commit.
       (use "git push" to publish your local commits)
    
     Changes to be committed:
    	modified:   bin/deploy.sh
    	modified:   bin/update_db.sh

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+# Execute this with 
+# > ./deploy.sh <key_name> 
+# where <key_name> is the name of the private key in ~/.ssh 
+# authorised to access the miracle AWS server. Once executed,
+# the latest changes from miracle-browser and miracle-db will
+# be pulled and deployed instantly to the public website.
+
 ssh -i ~/.ssh/$1 ec2-user@34.245.139.102 "cd distributed && ./run.sh && exit"
--- bin/update_db.sh
+++ bin/update_db.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+# Execute this with
+# > ./update_db.sh <key_name> <path_to_local_db> 
+# where <key_name> is the name of the private key in ~/.ssh authorised
+# to access the miracle AWS server and <path_to_local_db> is the database
+# file you have locally that you wish to push to miracle.
+
 scp -i ~/.ssh/$1 $2 ec2-user@34.245.139.102:/home/ec2-user/distributed/miracle.sqlite
